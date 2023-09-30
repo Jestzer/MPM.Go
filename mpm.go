1cf844a3dc39a610513bee7b4f024440e729696c
Jestzer <126624248+Jestzer@users.noreply.github.com>
2023-09-29 22:32:29 -0400

    Slowly getting there. Windows build is out of date here.

--- mpm.go
+++ mpm.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"runtime"
 )
@@ -9,35 +11,102 @@ import (
 func main() {
 	var operatingSystem string
 	var defaultTMP string
+	var mpmURL string
 
+	// Figure out your OS.
 	switch userOS := runtime.GOOS; userOS {
 	case "darwin":
 		operatingSystem = "maci64"
 		defaultTMP = "/tmp"
+		mpmURL = "https://www.mathworks.com/mpm/maci64/mpm"
 		fmt.Println("macOS")
 	case "windows":
 		operatingSystem = "win64"
 		defaultTMP = os.Getenv("TMP")
+		mpmURL = "https://www.mathworks.com/mpm/win64/mpm"
 		fmt.Println("Windows")
 	case "linux":
 		operatingSystem = "glnxa64"
 		defaultTMP = "/tmp"
+		mpmURL = "https://www.mathworks.com/mpm/glxna64/mpm"
 		fmt.Println("Linux")
 	default:
 		operatingSystem = "unknown"
-		defaultTMP = ""
-		fmt.Println("Unknown operating system")
 	}
 
+	if operatingSystem == "unknown" {
+		fmt.Println("Your operating system is unrecognized. Exiting.")
+		return
+	}
+
+	// Keeping these throughout for sanity's sake.
 	fmt.Println("Operating System:", operatingSystem)
+	fmt.Println("MPM URL:", mpmURL)
 
-	fmt.Print("Enter the path to the directory where you would like MPM to download to. Press Enter to use " + `"` + defaultTMP + `"` + ": ")
-	var mpmDownloadPath string
-	fmt.Scanln(&mpmDownloadPath)
+	// Figure out where you want actual MPM to go to.
+	for {
+		fmt.Print("Enter the path to the directory where you would like MPM to download to. Press Enter to use \"" + defaultTMP + "\": ")
+		var mpmDownloadPath string
+		fmt.Scanln(&mpmDownloadPath)
 
-	if mpmDownloadPath == "" {
-		mpmDownloadPath = defaultTMP
+		if mpmDownloadPath == "" {
+			mpmDownloadPath = defaultTMP
+		} else {
+			_, err := os.Stat(mpmDownloadPath)
+			if os.IsNotExist(err) {
+				fmt.Printf("The directory \"%s\" does not exist. Do you want to create it? (y/n): ", mpmDownloadPath)
+				var createDir string
+				fmt.Scanln(&createDir)
+				if createDir == "y" || createDir == "Y" {
+					err := os.MkdirAll(mpmDownloadPath, 0755)
+					if err != nil {
+						fmt.Println("Failed to create the directory:", err, "Please select a different directory.")
+						continue // Go back to the beginning of the loop
+					}
+					fmt.Println("Directory created successfully.")
+				} else {
+					fmt.Println("Directory creation skipped. Please select a different directory.")
+					continue // Go back to the beginning of the loop
+				}
+			} else if err != nil {
+				fmt.Println("Error checking the directory:", err, "Please select a different directory.")
+				continue // Go back to the beginning of the loop
+			}
+		}
+
+		fmt.Println("Download Path:", mpmDownloadPath)
+
+		// Download MPM. It needs to be in the loop because of mpmDownloadPath.
+		fileName := mpmDownloadPath + "/mpm"
+		err := downloadFile(mpmURL, fileName)
+		if err != nil {
+			fmt.Println("Failed to download the file:", err)
+			continue // Go back to the beginning of the loop
+		}
+		fmt.Println("File downloaded successfully.")
+
+		break // Exit the loop
+	}
+}
+
+// Function to download a file from the given URL and save it to the specified path.
+func downloadFile(url string, filePath string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
 	}
 
-	fmt.Println("Download Path:", mpmDownloadPath)
+	return nil
 }
