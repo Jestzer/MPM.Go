e56bfa0a3a540864ebe6d3e9601184faebe69078
Jestzer <126624248+Jestzer@users.noreply.github.com>
2023-09-29 20:42:45 -0400

    Small changes. Compiled a Windows build.

--- mpm.go
+++ mpm.go
@@ -2,26 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 )
 
 func main() {
 	var operatingSystem string
+	var defaultTMP string
 
-	switch os := runtime.GOOS; os {
+	switch userOS := runtime.GOOS; userOS {
 	case "darwin":
 		operatingSystem = "maci64"
+		defaultTMP = "/tmp"
 		fmt.Println("macOS")
 	case "windows":
 		operatingSystem = "win64"
+		defaultTMP = os.Getenv("TMP")
 		fmt.Println("Windows")
 	case "linux":
 		operatingSystem = "glnxa64"
+		defaultTMP = "/tmp"
 		fmt.Println("Linux")
 	default:
 		operatingSystem = "unknown"
+		defaultTMP = ""
 		fmt.Println("Unknown operating system")
 	}
 
 	fmt.Println("Operating System:", operatingSystem)
+
+	fmt.Print("Enter the path to the directory where you would like MPM to download to. Press Enter to use " + `"` + defaultTMP + `"` + ": ")
+	var mpmDownloadPath string
+	fmt.Scanln(&mpmDownloadPath)
+
+	if mpmDownloadPath == "" {
+		mpmDownloadPath = defaultTMP
+	}
+
+	fmt.Println("Download Path:", mpmDownloadPath)
 }
