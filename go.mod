9a8ce6aee1571922db1c7e1de45d8fb8d0f15c95
Jestzer <james@buonopane.email>
2023-10-02 20:53:16 -0400

    Extraction seems to mostly work now.

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/Jestzer/mpm-go/blob/main/mpm
 
 go 1.21.1
+//go 1.23
--- mpm.go
+++ mpm.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"archive/zip"
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func main() {
@@ -80,7 +82,7 @@ func main() {
 		fileName := mpmDownloadPath + "/mpm"
 		_, err := os.Stat(fileName)
 		if err == nil {
-			fmt.Printf("MPM already exists in this directory. Would you like to overwrite it? (y/n): ")
+			fmt.Printf("MPM already exists in this directory. Would you like to overwrite it? This will also overwrite the directory 'mpm-contents' (y/n): ")
 			var overwriteMPM string
 			fmt.Scanln(&overwriteMPM)
 			if overwriteMPM == "n" || overwriteMPM == "N" {
@@ -91,7 +93,7 @@ func main() {
 
 		fmt.Println("Beginning download of MPM. Please wait.")
 
-		// Download MPM
+		// Download MPM.
 		err = downloadFile(mpmURL, fileName)
 		if err != nil {
 			fmt.Println("Failed to download MPM. ", err)
@@ -99,20 +101,72 @@ func main() {
 		}
 		fmt.Println("MPM downloaded successfully.")
 
-		// Unzip the file if using Windows or macOS
-		if os := runtime.GOOS; os == "windows" || os == "darwin" {
-			unzipPath := mpmDownloadPath + "/mpm"
-			err := unzipFile(fileName, unzipPath)
+		// Unzip the file if using Windows or macOS.
+		if runtime.GOOS == "windows" || runtime.GOOS == "darwin" {
+			fmt.Println("Beginning extraction of MPM.")
+			unzipPath := filepath.Join(mpmDownloadPath, "mpm-contents")
+
+			// Check if the "mpm-contents" folder already exists
+			if _, err := os.Stat(unzipPath); err == nil {
+				// Delete the existing "mpm-contents" folder
+				err := os.RemoveAll(unzipPath)
+				if err != nil {
+					fmt.Println("Failed to delete the existing 'mpm-contents' directory:", err)
+					continue // Go back to the beginning of the loop
+				}
+			}
+
+			err := os.MkdirAll(unzipPath, 0755)
 			if err != nil {
-				fmt.Println("Failed to unzip the file:", err)
+				fmt.Println("Failed to create the directory:", err)
 				continue // Go back to the beginning of the loop
 			}
+
+			err = unzipFile(fileName, unzipPath)
+			if err != nil {
+				fmt.Println("Failed to unzip MPM:", err)
+				continue // Go back to the beginning of the loop
+			}
+
 			fmt.Println("MPM file unzipped successfully.")
 		}
 
 		break // Exit the loop
 	}
 
+	// Ask the user which release they'd like to install.
+	reader := bufio.NewReader(os.Stdin)
+	validReleases := []string{
+		"R2017b", "R2018a", "R2018b", "R2019a", "R2019b", "R2020a", "R2020b",
+		"R2021a", "R2021b", "R2022a", "R2022b", "R2023a", "R2023b",
+	}
+	defaultRelease := "R2023b"
+
+	for {
+		fmt.Printf("Enter which release you would like to install. Press Enter to select %s: ", defaultRelease)
+		release, _ := reader.ReadString('\n')
+		release = strings.TrimSpace(release)
+		if release == "" {
+			release = defaultRelease
+		}
+
+		release = strings.ToLower(release)
+		found := false
+		for _, validRelease := range validReleases {
+			if strings.ToLower(validRelease) == release {
+				release = validRelease
+				found = true
+				break
+			}
+		}
+
+		if found {
+			fmt.Println("Selected release:", release)
+			break
+		}
+
+		fmt.Println("Invalid release. Enter a release between R2017b-R2023b.")
+	}
 }
 
 // Function to download a file from the given URL and save it to the specified path.
@@ -148,9 +202,21 @@ func unzipFile(src, dest string) error {
 	for _, file := range reader.File {
 		path := filepath.Join(dest, file.Name)
 
+		// Reconstruct the path on Windows to ensure proper subdirectories are created.
+		// May also need to be done on macOS. Don't know yet.
+		if runtime.GOOS == "windows" {
+			path = filepath.Join(dest, file.Name)
+			path = filepath.FromSlash(path)
+		}
+
 		if file.FileInfo().IsDir() {
 			os.MkdirAll(path, file.Mode())
 			continue
+		}
+
+		err := os.MkdirAll(filepath.Dir(path), 0755)
+		if err != nil {
+			return err
 		}
 
 		fileReader, err := file.Open()
@@ -165,7 +231,8 @@ func unzipFile(src, dest string) error {
 		}
 		defer targetFile.Close()
 
-		if _, err := io.Copy(targetFile, fileReader); err != nil {
+		_, err = io.Copy(targetFile, fileReader)
+		if err != nil {
 			return err
 		}
 	}
