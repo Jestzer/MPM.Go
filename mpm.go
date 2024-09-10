ee6ae6533426be1efc8d8e4f80569416981f6fec
Jestzer <james@buonopane.email>
2024-09-10 08:22:54 -0400

    Updated version number with R2024b support. Still need to confirm that no new products have been added to non-Windows platforms.

--- mpm.go
+++ mpm.go
@@ -44,7 +44,7 @@ func main() {
 	args := os.Args[1:]
 	for _, arg := range args {
 		if arg == "-version" {
-			fmt.Println("Version number: 1.1")
+			fmt.Println("Version number: 1.2")
 			os.Exit(0)
 		}
 	}
